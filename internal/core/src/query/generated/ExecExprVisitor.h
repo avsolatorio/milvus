9cd6e4d51c391dc060b6ed73de90f5103b883f07
avsolatorio <avsolatorio@gmail.com>
2022-04-16 19:01:07 -0400

    Declare BinaryArithOpUnaryRangeExpr in ExecExprVisitor.h

--- internal/core/src/query/generated/ExecExprVisitor.h
+++ internal/core/src/query/generated/ExecExprVisitor.h
@@ -36,6 +36,9 @@ class ExecExprVisitor : public ExprVisitor {
     visit(UnaryRangeExpr& expr) override;
 
     void
+    visit(BinaryArithOpUnaryRangeExpr& expr) override;
+
+    void
     visit(BinaryRangeExpr& expr) override;
 
     void
