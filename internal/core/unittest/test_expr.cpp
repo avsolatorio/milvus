39cfcbb3699cb83546827f8dcd3a5ae5c2d21ca9
avsolatorio <avsolatorio@gmail.com>
2022-04-17 04:51:55 -0400

    Revert test case to proper version

--- internal/core/unittest/test_expr.cpp
+++ internal/core/unittest/test_expr.cpp
@@ -592,7 +592,7 @@ TEST(Expr, TestBinaryArithOpUnaryRange) {
                 "right_operand": 500,
                 "value": 2500
             }
-        })", [](int v) { return (v + 500) != 2500; }},
+        })", [](int v) { return (v + 500) == 2500; }},
         {R"("EQ": {
             "SUB": {
                 "right_operand": 500,
@@ -611,12 +611,12 @@ TEST(Expr, TestBinaryArithOpUnaryRange) {
                 "value": 1000
             }
         })", [](int v) { return (v / 2) == 1000; }},
-        // {R"("EQ": {
-        //     "MOD": {
-        //         "right_operand": 100,
-        //         "value": 1029
-        //     }
-        // })", [](int v) { return (v % 100) == 0; }},
+        {R"("EQ": {
+            "MOD": {
+                "right_operand": 100,
+                "value": 0
+            }
+        })", [](int v) { return (v % 100) == 0; }},
     };
 
     std::string dsl_string_tmp = R"({
