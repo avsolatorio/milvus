a9a43d6ba01126e0713fe687154d7836357baf3d
avsolatorio <avsolatorio@gmail.com>
2022-04-17 16:20:39 -0400

    Use reset instead of set in EvalNotIn

--- internal/core/src/index/ScalarIndexSort-inl.h
+++ internal/core/src/index/ScalarIndexSort-inl.h
@@ -169,24 +169,24 @@ ScalarIndexSort<T>::EvalNotIn(std::string arith_op, T right_operand, T value) {
 
     for (; lb < ub; ++lb) {
         if (arith_op == "add") {
-            if (lb->a_ + right_operand != value) {
-                bitset->set(lb->idx_);
+            if (lb->a_ + right_operand == value) {
+                bitset->reset(lb->idx_);
             }
         } else if (arith_op == "sub") {
-            if (lb->a_ - right_operand != value) {
-                bitset->set(lb->idx_);
+            if (lb->a_ - right_operand == value) {
+                bitset->reset(lb->idx_);
             }
         } else if (arith_op == "mul") {
-            if (lb->a_ * right_operand != value) {
-                bitset->set(lb->idx_);
+            if (lb->a_ * right_operand == value) {
+                bitset->reset(lb->idx_);
             }
         } else if (arith_op == "div") {
-            if (lb->a_ / right_operand != value) {
-                bitset->set(lb->idx_);
+            if (lb->a_ / right_operand == value) {
+                bitset->reset(lb->idx_);
             }
         } else if (arith_op == "mod") {
-            if (static_cast<T>(fmod(lb->a_, right_operand)) != value) {
-                bitset->set(lb->idx_);
+            if (static_cast<T>(fmod(lb->a_, right_operand)) == value) {
+                bitset->reset(lb->idx_);
             }
         } else {
             throw std::invalid_argument("ScalarIndexSort::EvalNotIn: invalid arith_op");
