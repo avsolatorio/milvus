2180895862ff685adf29761fccc314524f851c6d
Aivin V. Solatorio <avsolatorio@gmail.com>
2022-04-18 18:52:51 -0400

    Update test_expr tests to improve code coverage
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- internal/core/unittest/test_expr.cpp
+++ internal/core/unittest/test_expr.cpp
@@ -585,69 +585,87 @@ TEST(Expr, TestCompare) {
 TEST(Expr, TestBinaryArithOpEvalRange) {
     using namespace milvus::query;
     using namespace milvus::segcore;
-    std::vector<std::tuple<std::string, std::function<bool(int)>>> testcases = {
+    std::vector<std::tuple<std::string, std::function<bool(int)>, DataType>> testcases = {
         // Add test cases for BinaryArithOpEvalRangeExpr EvalEq
         {R"("EQ": {
             "ADD": {
                 "right_operand": 500,
                 "value": 2500
             }
-        })", [](int v) { return (v + 500) == 2500; }},
+        })", [](int8_t v) { return (v + 500) == 2500; }, DataType::INT8},
         {R"("EQ": {
             "SUB": {
                 "right_operand": 500,
                 "value": 1500
             }
-        })", [](int v) { return (v - 500) == 1500; }},
+        })", [](int16_t v) { return (v - 500) == 1500; }, DataType::INT16},
         {R"("EQ": {
             "MUL": {
                 "right_operand": 2,
                 "value": 4000
             }
-        })", [](int v) { return (v * 2) == 4000; }},
+        })", [](int32_t v) { return (v * 2) == 4000; }, DataType::INT32},
         {R"("EQ": {
             "DIV": {
                 "right_operand": 2,
                 "value": 1000
             }
-        })", [](int v) { return (v / 2) == 1000; }},
+        })", [](int64_t v) { return (v / 2) == 1000; }, DataType::INT64},
         {R"("EQ": {
             "MOD": {
                 "right_operand": 100,
                 "value": 0
             }
-        })", [](int v) { return (v % 100) == 0; }},
+        })", [](int32_t v) { return (v % 100) == 0; }, DataType::INT32},
+        {R"("EQ": {
+            "ADD": {
+                "right_operand": 500,
+                "value": 2500
+            }
+        })", [](float v) { return (v + 500) == 2500; }, DataType::FLOAT},
+        {R"("EQ": {
+            "ADD": {
+                "right_operand": 500,
+                "value": 2500
+            }
+        })", [](double v) { return (v + 500) == 2500; }, DataType::DOUBLE},
         // Add test cases for BinaryArithOpEvalRangeExpr EvalNotEq
         {R"("NE": {
             "ADD": {
                 "right_operand": 500,
                 "value": 2500
             }
-        })", [](int v) { return (v + 500) != 2500; }},
+        })", [](float v) { return (v + 500) != 2500; }, DataType::FLOAT},
         {R"("NE": {
             "SUB": {
                 "right_operand": 500,
-                "value": 1500
+                "value": 2500
             }
-        })", [](int v) { return (v - 500) != 1500; }},
+        })", [](double v) { return (v - 500) != 2500; }, DataType::DOUBLE},
         {R"("NE": {
             "MUL": {
                 "right_operand": 2,
                 "value": 4000
             }
-        })", [](int v) { return (v * 2) != 4000; }},
+        })", [](int8_t v) { return (v * 2) != 4000; }, DataType::INT8},
         {R"("NE": {
             "DIV": {
                 "right_operand": 2,
                 "value": 1000
             }
-        })", [](int v) { return (v / 2) != 1000; }},
+        })", [](int16_t v) { return (v / 2) != 1000; }, DataType::INT16},
         {R"("NE": {
             "MOD": {
                 "right_operand": 100,
                 "value": 0
             }
-        })", [](int v) { return (v % 100) != 0; }},
+        })", [](int32_t v) { return (v % 100) != 0; }, DataType::INT32},
+        {R"("NE": {
+            "ADD": {
+                "right_operand": 500,
+                "value": 2500
+            }
+        })", [](int64_t v) { return (v + 500) != 2500; }, DataType::INT64},
     };
 
     std::string dsl_string_tmp = R"({
@@ -655,9 +673,7 @@ TEST(Expr, TestBinaryArithOpEvalRange) {
             "must": [
                 {
                     "range": {
-                        "age": {
-                            @@@@
-                        }
+                        @@@@@
                     }
                 },
                 {
@@ -676,27 +692,98 @@ TEST(Expr, TestBinaryArithOpEvalRange) {
             ]
         }
     })";
+
+    std::string dsl_string_int8 = R"(
+        "age8": {
+            @@@@
+        })";
+
+    std::string dsl_string_int16 = R"(
+        "age16": {
+            @@@@
+        })";
+
+    std::string dsl_string_int32 = R"(
+        "age32": {
+            @@@@
+        })";
+
+    std::string dsl_string_int64 = R"(
+        "age64": {
+            @@@@
+        })";
+
+
+    std::string dsl_string_float = R"(
+        "age_float": {
+            @@@@
+        })";
+
+    std::string dsl_string_double = R"(
+        "age_double": {
+            @@@@
+        })";
+
     auto schema = std::make_shared<Schema>();
     schema->AddDebugField("fakevec", DataType::VECTOR_FLOAT, 16, MetricType::METRIC_L2);
-    schema->AddDebugField("age", DataType::INT32);
+    schema->AddDebugField("age8", DataType::INT8);
+    schema->AddDebugField("age16", DataType::INT16);
+    schema->AddDebugField("age32", DataType::INT32);
+    schema->AddDebugField("age64", DataType::INT64);
+    schema->AddDebugField("age_float", DataType::FLOAT);
+    schema->AddDebugField("age_double", DataType::DOUBLE);
 
     auto seg = CreateGrowingSegment(schema);
     int N = 1000;
-    std::vector<int> age_col;
+    std::vector<int8_t> age8_col;
+    std::vector<int16_t> age16_col;
+    std::vector<int32_t> age32_col;
+    std::vector<int64_t> age64_col;
+    std::vector<float> age_float_col;
+    std::vector<double> age_double_col;
     int num_iters = 100;
     for (int iter = 0; iter < num_iters; ++iter) {
         auto raw_data = DataGen(schema, N, iter);
-        auto new_age_col = raw_data.get_col<int>(1);
-        age_col.insert(age_col.end(), new_age_col.begin(), new_age_col.end());
+
+        auto new_age8_col = raw_data.get_col<int8_t>(1);
+        auto new_age16_col = raw_data.get_col<int16_t>(2);
+        auto new_age32_col = raw_data.get_col<int32_t>(3);
+        auto new_age64_col = raw_data.get_col<int64_t>(4);
+        auto new_age_float_col = raw_data.get_col<float>(5);
+        auto new_age_double_col = raw_data.get_col<double>(6);
+
+        age8_col.insert(age8_col.end(), new_age8_col.begin(), new_age8_col.end());
+        age16_col.insert(age16_col.end(), new_age16_col.begin(), new_age16_col.end());
+        age32_col.insert(age32_col.end(), new_age32_col.begin(), new_age32_col.end());
+        age64_col.insert(age64_col.end(), new_age64_col.begin(), new_age64_col.end());
+        age_float_col.insert(age_float_col.end(), new_age_float_col.begin(), new_age_float_col.end());
+        age_double_col.insert(age_double_col.end(), new_age_double_col.begin(), new_age_double_col.end());
+
         seg->PreInsert(N);
         seg->Insert(iter * N, N, raw_data.row_ids_.data(), raw_data.timestamps_.data(), raw_data.raw_);
     }
 
     auto seg_promote = dynamic_cast<SegmentGrowingImpl*>(seg.get());
     ExecExprVisitor visitor(*seg_promote, seg_promote->get_row_count(), MAX_TIMESTAMP);
-    for (auto [clause, ref_func] : testcases) {
-        auto loc = dsl_string_tmp.find("@@@@");
+    for (auto [clause, ref_func, dtype] : testcases) {
+        auto loc = dsl_string_tmp.find("@@@@@");
         auto dsl_string = dsl_string_tmp;
+        if (dtype == DataType::INT8) {
+            dsl_string.replace(loc, 5, dsl_string_int8);
+        if (dtype == DataType::INT16) {
+            dsl_string.replace(loc, 5, dsl_string_int16);
+        if (dtype == DataType::INT32) {
+            dsl_string.replace(loc, 5, dsl_string_int32);
+        if (dtype == DataType::INT64) {
+            dsl_string.replace(loc, 5, dsl_string_int64);
+        } else if (dtype == DataType::FLOAT) {
+            dsl_string.replace(loc, 5, dsl_string_float);
+        } else if (dtype == DataType::DOUBLE) {
+            dsl_string.replace(loc, 5, dsl_string_double);
+        } else {
+            ASSERT_TRUE(false) << "No test case defined for this data type";
+        }
+        loc = dsl_string.find("@@@@");
         dsl_string.replace(loc, 4, clause);
         auto plan = CreatePlan(*schema, dsl_string);
         auto final = visitor.call_child(*plan->plan_node_->predicate_.value());
@@ -704,7 +791,22 @@ TEST(Expr, TestBinaryArithOpEvalRange) {
 
         for (int i = 0; i < N * num_iters; ++i) {
             auto ans = final[i];
-            auto val = age_col[i];
+
+            if (dtype == DataType::INT8) {
+                auto val = age8_col[i];
+            } else if (dtype == DataType::INT16) {
+                auto val = age16_col[i];
+            } else if (dtype == DataType::INT32) {
+                auto val = age32_col[i];
+            } else if (dtype == DataType::INT64) {
+                auto val = age64_col[i];
+            } else if (dtype == DataType::FLOAT) {
+                auto val = age_float_col[i];
+            } else if (dtype == DataType::DOUBLE) {
+                auto val = age_double_col[i];
+            } else {
+                ASSERT_TRUE(false) << "No test case defined for this data type";
+            }
             auto ref = ref_func(val);
             ASSERT_EQ(ans, ref) << clause << "@" << i << "!!" << val << "@" << ans << "!!" << ref;
         }
